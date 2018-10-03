b953541a2811fc14ff765d54b5d3a45c57c6c3f2
KaniRobinson <me@kanirobinson.co.uk>
2018-10-03 11:45:38 +0100

    Added Update Method

--- src/actions/Update.js
+++ src/actions/Update.js
@@ -1,7 +1,32 @@
+import Axios from '../orm/axios';
 import Action from './Action'
+import Context from '../common/context'
 
 export default class Update extends Action {
-  static call ({ state, dispatch }, params={}) {
-    console.log(state, dispatch, params);
+  /**
+   * Call $update method
+   * @param {object} store
+   * @param {object} params
+   */
+  static async call ({ state, commit }, params = {}) {
+    if(!params.data || typeof params !== 'object') {
+      throw new TypeError("You must include a data object in the params to send a POST request", params)
+    }
+
+    const context = Context.getInstance();
+    const model = context.getModelFromState(state);
+    const endpoint = Action.transformParams('$update', model, params);
+    const axios =  new Axios(model.methodConf.http);
+    const request = axios.put(endpoint, params.data);
+
+    request
+      .then(data => {
+        model.update({
+          where: data.id,
+          data
+        })
+      })
+
+    return request;
   }
 }
