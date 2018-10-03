76cd16523d9226cc4a4b60a7011d32db5c54d224
KaniRobinson <me@kanirobinson.co.uk>
2018-10-03 10:07:37 +0100

    Added Fetch Method

--- src/actions/Fetch.js
+++ src/actions/Fetch.js
@@ -1,10 +1,32 @@
+import Axios from '../orm/axios';
 import Action from './Action'
 import Context from '../common/context'
 
 export default class Fetch extends Action {
-  static async call ({ state }, params = {}) {
+  /**
+   * Call $fetch method
+   * @param {object} store
+   * @param {object} params
+   */
+  static async call ({ state, commit }, params = {}) {
     const context = Context.getInstance();
     const model = context.getModelFromState(state);
     const endpoint = Action.transformParams('$fetch', model, params);
+    const axios =  new Axios(model.methodConf.http);
+    const request = axios.get(endpoint);
+
+    commit('onRequest');
+    request
+      .then(data => {
+        commit('onSuccess')
+        model.insertOrUpdate({
+          data,
+        });
+      })
+      .catch(error => {
+        commit('onError', error)
+      })
+
+    return request;
   }
 }
