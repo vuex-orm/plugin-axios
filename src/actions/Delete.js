1e1a76a7fff699bae9f3ab2e55c795d043c94dca
KaniRobinson <me@kanirobinson.co.uk>
2018-10-03 11:46:04 +0100

    Added Delete Method

--- src/actions/Delete.js
+++ src/actions/Delete.js
@@ -1,7 +1,27 @@
+import Axios from '../orm/axios';
 import Action from './Action'
+import Context from '../common/context'
 
 export default class Delete extends Action {
-  static call ({ state, dispatch }, params={}) {
-    console.log(state, dispatch, params);
+  /**
+   * Call $delete method
+   * @param {object} store
+   * @param {object} params
+   */
+  static async call ({ state, commit }, params = {}) {
+    const context = Context.getInstance();
+    const model = context.getModelFromState(state);
+    const endpoint = Action.transformParams('$delete', model, params);
+    const axios =  new Axios(model.methodConf.http);
+    const request = axios.delete(endpoint);
+
+    request
+      .then(data => {
+        model.delete({
+          where: (params.params.id) ? params.params.id : data.id,
+        })
+      })
+
+    return request;
   }
 }
