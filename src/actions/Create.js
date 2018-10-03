fc892c91a02004906aab2ab581e11597efe662a8
KaniRobinson <me@kanirobinson.co.uk>
2018-10-03 11:45:27 +0100

    Added Create Method

--- src/actions/Create.js
+++ src/actions/Create.js
@@ -1,7 +1,34 @@
+import Axios from '../orm/axios';
 import Action from './Action'
+import Context from '../common/context'
 
 export default class Create extends Action {
-  static call ({ state, dispatch }, params = {}) {
-    // console.log(state, dispatch, params);
+  /**
+   * Call $create method
+   * @param {object} store
+   * @param {object} params
+   */
+  static async call ({ state, commit }, params = {}) {
+    if(!params.data || typeof params !== 'object') {
+      throw new TypeError("You must include a data object in the params to send a POST request", params)
+    }
+
+    const context = Context.getInstance();
+    const model = context.getModelFromState(state);
+    const endpoint = Action.transformParams('$create', model, params);
+    const axios =  new Axios(model.methodConf.http);
+    const request = axios.post(endpoint, params.data);
+
+    commit('onRequest');
+    request
+      .then(data => {
+        commit('onSuccess')
+        model.insertOrUpdate({
+          data,
+        });
+      })
+      .catch(error => commit('onError', error))
+
+    return request;
   }
 }
